34234c1475aec840d81e2d253a5f3b26b957c6c9
Pedro Henrique <pedruhbl@yahoo.com.br>
2023-09-28 23:48:03 -0300

    Feat: Create methods for all events

--- static/popup.js
+++ static/popup.js
@@ -1,92 +1,75 @@
-import { CreateCNPJ } from '../src/model/CNPJ';
-import { CreateCPF } from '../src/model/CPF';
+import {CreateCNPJ} from '../src/model/CNPJ';
+import {CreateCPF} from '../src/model/CPF';
 
-function initializePopup() {
-    document.getElementById('generateCNPJ').addEventListener('click', function () {
-      const newCnpj = new CreateCNPJ();
-      const generatedCnpj = newCnpj.generateCnpj();
-
-      const cnpjContainer = document.getElementById('cnpjContainer');
-      cnpjContainer.textContent = 'Generated CNPJ: ' + generatedCnpj;
-      cnpjContainer.style.display = 'block';
-
-      const cpfContainer = document.getElementById('cpfContainer');
-      cpfContainer.style.display = 'none';
-
-      const copyButton = document.getElementById('copyValue');
-      copyButton.style.display = 'block';
-
-      const backButton = document.getElementById('backButton');
-      backButton.style.display = 'block';
-
-      copyButton.addEventListener('click', function () {
-        const textarea = document.createElement('textarea');
-        textarea.value = generatedCnpj;
-        document.body.appendChild(textarea);
-        textarea.select();
-        document.execCommand('copy');
-        document.body.removeChild(textarea);
-
-        const copyMessage = document.getElementById('copyMessage');
-        copyMessage.textContent = 'CNPJ Copied!';
-        copyMessage.style.display = 'block';
-
-        setTimeout(function () {
-          copyMessage.style.display = 'none';
-        }, 5000);
-      });
-
-      backButton.addEventListener('click', function () {
-        cnpjContainer.style.display = 'none';
-        options.style.display = 'block';
-        copyButton.style.display = 'none';
-        backButton.style.display = 'none';
-        document.getElementById('copyMessage').style.display = 'none';
-      });
-    });
-
-  document.getElementById('generateCPF').addEventListener('click', function () {
-    const newCpf = new CreateCPF();
-    const generatedCpf = newCpf.generateCpf();
-
+function initializePopup () {
     const cpfContainer = document.getElementById('cpfContainer');
-    cpfContainer.textContent = 'Generated CPF: ' + generatedCpf;
-    cpfContainer.style.display = 'block';
-
     const cnpjContainer = document.getElementById('cnpjContainer');
-    cnpjContainer.style.display = 'none';
-
     const copyButton = document.getElementById('copyValue');
-    copyButton.style.display = 'block';
-
     const backButton = document.getElementById('backButton');
-    backButton.style.display = 'block';
+    const typeCpf = 'CPF'
+    const typeCnpj = 'CNPJ'
 
-    copyButton.addEventListener('click', function () {
-      const textarea = document.createElement('textarea');
-      textarea.value = generatedCpf;
-      document.body.appendChild(textarea);
-      textarea.select();
-      document.execCommand('copy');
-      document.body.removeChild(textarea);
+    document.getElementById('generateCNPJ').addEventListener('click', function () {
+        const newCnpj = new CreateCNPJ();
+        const generatedCnpj = newCnpj.generateCnpj();
 
-      const copyMessage = document.getElementById('copyMessage');
-      copyMessage.textContent = 'CPF Copied!';
-      copyMessage.style.display = 'block';
+        handleElementsStyle(cnpjContainer, cpfContainer, copyButton, backButton, generatedCnpj, typeCnpj);
 
-      setTimeout(function () {
+        copyButton.addEventListener('click', function () {
+            actionCopyButton(generatedCnpj, typeCnpj);
+        });
+
+        backButton.addEventListener('click', function () {
+            handleBackButton(cnpjContainer, copyButton, backButton);
+        });
+    });
+
+    document.getElementById('generateCPF').addEventListener('click', function () {
+        const newCpf = new CreateCPF();
+        const generatedCpf = newCpf.generateCpf();
+
+        handleElementsStyle(cpfContainer, cnpjContainer, copyButton, backButton, generatedCpf, typeCpf);
+
+        copyButton.addEventListener('click', function () {
+
+            actionCopyButton(generatedCpf, typeCpf);
+        });
+
+        backButton.addEventListener('click', function () {
+            handleBackButton(cpfContainer, copyButton, backButton);
+        });
+    });
+}
+
+const handleElementsStyle = (visibleContainer, hiddenContainer, copyBtn, backBtn, dataGenerated, type) => {
+    visibleContainer.textContent = `Generated ${type}: ${dataGenerated}`;
+    visibleContainer.style.display = 'block';
+    hiddenContainer.style.display = 'none';
+    copyBtn.style.display = 'block';
+    backBtn.style.display = 'block';
+}
+
+const actionCopyButton = (generateData, type) => {
+    const textarea = document.createElement('textarea');
+    textarea.value = generateData;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+    const copyMessage = document.getElementById('copyMessage');
+    copyMessage.textContent = `${type} Copied!`;
+    copyMessage.style.display = 'block';
+
+    setTimeout(function () {
         copyMessage.style.display = 'none';
-      }, 5000);
-    });
+    }, 5000);
+}
 
-    backButton.addEventListener('click', function () {
-      cpfContainer.style.display = 'none';
-      options.style.display = 'block';
-      copyButton.style.display = 'none';
-      backButton.style.display = 'none';
-      document.getElementById('copyMessage').style.display = 'none';
-    });
-  });
+const handleBackButton = (visibleContainer, copyButton, backButton) => {
+    visibleContainer.style.display = 'none';
+    copyButton.style.display = 'none';
+    backButton.style.display = 'none';
+    document.getElementById('copyMessage').style.display = 'none';
 }
 
 initializePopup();
